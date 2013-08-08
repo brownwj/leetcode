a515c67cbc2e1aee5306827b3885374425984fba
brownwj <brownwj.cn@gmail.com>
2013-08-08 17:40:07 +0800

    template

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+
+class Solution
+{
+//write your function to calculate here
+public:
+
+};
+
+
+int main()
+{
+	Solution* my_solution = new Solution();
+//declare and generate your input here
+
+//solute your problem here
+	my_solution->solve();
+
+//print your output here
+    cout<<endl;
+}
