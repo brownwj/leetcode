cda6c50b450078a50561f86412b9bd3809ea9b32
wangjia <wangjia@localhost.localdomain>
2013-08-11 03:59:57 -0400

    small 8ms, large 140ms

---
+++ reverseNodesInKGroup.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(k<2) return head;
+        int length=0;
+        ListNode* temp = head;
+        while(temp)
+        {
+            temp = temp->next;
+            length++;
+        }
+        int x=length/k;
+        int y=length%k;
+        for(int i=0;i<x;i++)
+        {
+            head=reverseBetween(head,k*i+1,k*i+k);
+        }
+        return head;
+    }
+    
+        ListNode *reverseBetween(ListNode *head, int m, int n) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        ListNode temp_head(1);
+        temp_head.next = head;
+        ListNode* begin = &temp_head;
+        int i=1;
+        while(i<m)
+        {
+            begin=begin->next;
+            i++;
+        }
+        ListNode *current = begin->next->next;
+        ListNode *current_pre = begin->next;
+        while(i<n)
+        {
+            ListNode *tmp1 = current->next;
+            current_pre->next = tmp1;
+            ListNode *tmp2 = begin->next;
+            begin->next = current;
+            current->next = tmp2;
+            
+            current = current_pre->next;
+            i++;
+        }
+        return temp_head.next;
+    }
+};
