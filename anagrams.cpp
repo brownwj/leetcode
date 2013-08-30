acf77c920b1d13d615afbd3888a9150e55919664
wangjia <wangjia@localhost.localdomain>
2013-08-30 05:00:12 -0400

    small 12ms, large 228ms

---
+++ anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> anagrams(vector<string> &strs) {
+        vector<string> res;
+        if (strs.size() <= 1) return res;
+        map<string, int> anagram;
+        for (int i = 0; i < strs.size(); ++i)
+        {
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            if (anagram.find(s) == anagram.end()) {
+                anagram.insert(make_pair(s, i));
+            } else {
+                if (anagram[s] >= 0) {
+                    res.push_back(strs[anagram[s]]);
+                    anagram[s] = -1;
+                }
+                res.push_back(strs[i]);
+            }
+        }
+        return res;
+    }
+};
